b16f629df26d5f6c74bfb90033887a15f7ec9788
Jiri Pospisil <jiri@jpospisil.com>
2023-06-21 16:33:59 +0000

    Dont lose the message

--- update_pages.bash
+++ update_pages.bash
@@ -7,7 +7,9 @@ wget https://github.com/tldr-pages/tldr/archive/main.zip
 rm -rf pages
 unzip main.zip "tldr-main/pages/*.md"
 mv tldr-main/pages .
+
 date -u +%Y-%m-%d > pages/updated_on
+echo "These pages are copied from https://github.com/tldr-pages/tldr and are licensed under the Creative Commons Attribution 4.0 International License (CC-BY)." > pages/README
 
 rmdir tldr-main
 rm main.zip
