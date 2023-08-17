32adcc22331f045dbf819b2c4a338457a4eafd5a
Jiri Pospisil <jiri@jpospisil.com>
2023-08-17 14:28:32 +0000

    Update pages

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const VERSION = "0.0.7";
+const VERSION = "0.0.8";
 
 fn embedData(b: *std.Build, exe: *std.Build.Step.Compile) !void {
     var options = b.addOptions();
