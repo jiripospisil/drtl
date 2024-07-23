f93ac5e59cfdfa25385da754fa8c5b107ef6fab7
Jiri Pospisil <jiri@jpospisil.com>
2024-07-23 12:41:29 +0000

    Go from 5s to 160ms with BufferedWriter

--- src/compile_pages.zig
+++ src/compile_pages.zig
@@ -7,27 +7,29 @@ pub fn main() !void {
 
     const args = try std.process.argsAlloc(arena);
     const output_file = try std.fs.cwd().createFile(args[1], .{});
+    var writer = std.io.bufferedWriter(output_file.writer());
+    defer writer.flush() catch unreachable;
 
-    try writePages(arena, output_file);
+    try writePages(arena, writer.writer().any());
 }
 
-fn writePages(allocator: std.mem.Allocator, output_file: std.fs.File) !void {
-    try output_file.writeAll(
+fn writePages(allocator: std.mem.Allocator, writer: std.io.AnyWriter) !void {
+    try writer.writeAll(
         \\ const std = @import("std");
         \\
         \\ pub const pages = std.StaticStringMap([]const u8).initComptime(.{
         \\
     );
 
-    try traverseDir(allocator, output_file, "pages");
+    try traverseDir(allocator, writer, "pages");
 
-    try output_file.writeAll(
+    try writer.writeAll(
         \\ });
         \\
     );
 }
 
-fn traverseDir(allocator: std.mem.Allocator, output_file: std.fs.File, path: []const u8) !void {
+fn traverseDir(allocator: std.mem.Allocator, writer: std.io.AnyWriter, path: []const u8) !void {
     var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
     var it = dir.iterate();
 
@@ -44,7 +46,6 @@ fn traverseDir(allocator: std.mem.Allocator, output_file: std.fs.File, path: []c
                 defer f.close();
                 const content = try f.readToEndAlloc(allocator, 4096);
 
-                const writer = output_file.writer();
                 try writer.writeAll(".{\"");
                 try writer.writeAll(name[6..(name.len - 3)]);
                 try writer.writeAll("\", \"");
@@ -52,7 +53,7 @@ fn traverseDir(allocator: std.mem.Allocator, output_file: std.fs.File, path: []c
                 try writer.writeAll("\"}, \n");
             },
             .directory => {
-                try traverseDir(allocator, output_file, name);
+                try traverseDir(allocator, writer, name);
             },
             else => |t| std.debug.panic("Unexpected type '{any}'", .{t}),
         }
